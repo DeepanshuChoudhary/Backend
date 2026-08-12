0abb9d2961a2dac1530585a3323074167a55c51c
Deepanshu Choudhary <deepengineeryt@gmail.com>
2026-08-12 18:33:21 +0530

    Made some changes in the 2nd folder

--- 2. Building Server with Express/server.js
+++ 2. Building Server with Express/server.js
@@ -3,29 +3,15 @@ const path = require('path');
 
 const app = express();
 
-// Serve static files (CSS, JS, images) from current directory
-app.use(express.static(__dirname));
+const app = express()
 
 app.get('/home', (req, res) => {
-    res.send(`
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <title>Home Page</title>
-            <link rel="stylesheet" href="/style.css">
-        </head>
-        <body>
-            <h1>Welcome to the Home Page</h1>
-        </body>
-        </html>
-    `);
-});
+    res.send("Welcome to the Home Page")
+})
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-});
-
-
+    console.log("Server is running on port 3000")
+})
 
 
 
