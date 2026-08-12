7f666373a65787a6bc9aaf865a2e43129239b066
Deepanshu Choudhary <deepengineeryt@gmail.com>
2026-08-12 22:22:04 +0530

    REST api

--- 3. REST Api and Database/server.js
+++ 3. REST Api and Database/server.js
@@ -1,17 +1,65 @@
 const express = require('express')
 
-const app = express()
+const app = express();
+app.use(express.json())
 
-app.get('/home', (req, res) => {
-    console.log("Hello home - terminal")
-    res.send("Hello home")
+let notes = []
+
+app.post('/notes', (req,res) => {
+    console.log(req.body)
+    notes.push(req.body)
+    res.status(200).json({
+        message: "Notes received successfully",
+        notes: notes
+    })
 })
 
-app.get('/about', (req, res) => {
-    console.log("Hello About Page - terminal")
-    res.send("Hello About Page")
-})
-
-app.listen(3000, () => {
+app.listen(3000, (req, res) => {
     console.log("Server is running on port 3000")
 })
+
+
+
+
+
+
+
+
+
+
+// const express = require('express');
+
+// const app = express()
+
+// app.get('/home', (req, res) => {
+//     console.log("Home page")
+//     res.send("Hello Home")
+// })
+
+// app.listen(3000, () => {
+//     console.log("Server is running on port 3000")
+// })
+
+
+
+
+
+
+
+// const express = require('express')
+
+// const app = express()
+
+// app.get('/home', (req, res) => {
+//     console.log("Hello home - terminal")
+//     res.send("Hello home")
+// })
+
+// app.get('/about', (req, res) => {
+//     console.log("Hello About Page - terminal")
+//     res.send("Hello About Page")
+// })
+
+// app.listen(3000, () => {
+//     console.log("Server is running on port 3000")
+// })
