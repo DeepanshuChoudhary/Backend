839a9f854c352507c54e9c6e376034c94490df0c
Deepanshu Choudhary <deepengineeryt@gmail.com>
2026-08-20 17:48:31 +0530

    middleware

--- 7. Middleware/src/app.js
+++ 7. Middleware/src/app.js
@@ -4,6 +4,11 @@ const indexRoutes = require('./routes/index.routes')
 const app = express()
 
 app.use(express.json())
+
+app.use((req,res, next) => {
+    console.log("This is a Middleware in between app and route")
+    next()
+})
 app.use('/', indexRoutes)
 
 module.exports = app
--- 7. Middleware/src/routes/index.routes.js
+++ 7. Middleware/src/routes/index.routes.js
@@ -2,8 +2,13 @@ const express = require('express')
 
 const router = express.Router();
 
+router.use((req, res, next) => {
+    console.log("This is 2nd middleware in between router and API")
+    next()
+})
+
 router.get('/', (req, res) => {
-    console.log("This is .get router")
+    console.log("Welcome to the API")
     res.status(200).json({
         message: "Welcome to the API"
     })
