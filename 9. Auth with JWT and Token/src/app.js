58ac38273f287646d97627faa6871372c0ab4460
Deepanshu Choudhary <deepengineeryt@gmail.com>
2026-08-21 15:11:24 +0530

    cookies implemented

--- 9. Auth with JWT and Token/src/app.js
+++ 9. Auth with JWT and Token/src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const connectDB = require('./db/db')
 const userRouter = require('./routes/user.routes')
+const cookieParser = require("cookie-parser");
 
 const app = express()
 connectDB()
 
 app.use(express.json())
+app.use(cookieParser())
 app.use('/auth', userRouter)
 
 
--- 9. Auth with JWT and Token/src/routes/user.routes.js
+++ 9. Auth with JWT and Token/src/routes/user.routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const userModel = require('../models/user.model');
+const jwt = require('jsonwebtoken');
 
 const router = express.Router()
 
@@ -7,13 +8,19 @@ router.post('/register', async (req, res) => {
 
     const { username, password } = req.body
 
-    const data = await userModel.create({
+    const user = await userModel.create({
         username, password
     })
 
+    const token = jwt.sign({
+        id: user._id,
+    }, process.env.JWT_SECRET)
+
+    res.cookie('token', token)
+
     res.status(201).json({
         message: "Register account created successfully",
-        data: data
+        user
     })
 
 })
@@ -23,26 +30,64 @@ router.post('/login', async (req, res) => {
     const { username, password } = req.body
 
     const isUserExist = await userModel.findOne({
-        username
+        username, password
     })
 
-    if(!isUserExist) {
+    if (!isUserExist) {
         return res.status(401).json({
-            message: "Invalid username"
+            message: "Invalid input"
         })
     }
 
-    const passwordValid = password == isUserExist.password
+    // const passwordValid = password == isUserExist.password
 
-    if(!passwordValid) {
-        return res.status(401).json({
-            message: "Invalid password"
-        })
-    }
+    // if(!passwordValid) {
+    //     return res.status(401).json({
+    //         message: "Invalid password"
+    //     })
+    // }
 
     res.status(200).json({
         message: "Login"
     })
+
+})
+
+router.get('/user', async (req, res) => {
+
+    const { token } = req.cookies
+
+    if(!token) {
+        return res.status(401).json({
+            message: "Unauthorized access, token not available"
+        })
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+        const user = await userModel.findOne({
+            _id: decoded.id
+        }).select("-password -__v").lean()
+
+        res.status(200).json({
+            message: "User data fetch successfully",
+            user
+        })
+
+        // res.send(decoded)
+
+    }catch(err) {
+        return res.status(401).json({
+            message: "Unauthorized - Invalid Token"
+        })
+    }
+
+
+    // res.status(200).json({
+    //     message: "User login successfully",
+    //     decoded
+    // })
 
 })
 
