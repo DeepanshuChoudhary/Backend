ef8eceb252205cb8a250eebc4f8925b35a8417e3
Deepanshu Choudhary <deepengineeryt@gmail.com>
2026-08-29 16:23:44 +0530

    folder 13 ai.service.js if i paste the entire api key then works if i do process.env they don't work

--- 13. Project-3 Cloud Storage Provider Setup/src/service/ai.service.js
+++ 13. Project-3 Cloud Storage Provider Setup/src/service/ai.service.js
@@ -1,23 +1,28 @@
 require('dotenv').config()
-const { GoogleGenAI } = require('@google/genai')
+const { GoogleGenAI } = require("@google/genai")
 
 const ai = new GoogleGenAI({
-    apiKey: process.env.GEMINI_API_KEY
+    apiKey: "AIzaSyBd_SEYWNzAYqFx-V8WZPxjhmNbaRuhDrs"
 });
 
 const generateAIResponse = async (req, res) => {
 
-    try {
+    try{
         const interaction = await ai.interactions.create({
             model: "gemini-3.7-flash",
             input: "Explain how AI works in a few words"
         })
+        // console.log(response.text)
+        console.log(interaction.output_text)
     }
     catch(err) {
-        console.log("AI server error : ", err)
+        console.log("AI Error: ", err)
     }
 
 }
+
+generateAIResponse()
+
 
 // const interaction = await ai.interactions.create({
 //     model: "gemini-3.7-flash",
