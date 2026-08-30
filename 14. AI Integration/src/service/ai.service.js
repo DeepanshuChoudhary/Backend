915d12d83c879b435e4193ab8a6f58c311d0ae91
Deepanshu Choudhary <deepengineeryt@gmail.com>
2026-08-30 20:03:09 +0530

    ai.server.js updated system instruction

--- 14. AI Integration/src/service/ai.service.js
+++ 14. AI Integration/src/service/ai.service.js
@@ -5,19 +5,25 @@ const ai = new GoogleGenAI({
 })
 
 const generateCaption = async (base64ImageFile) => {
-    
+
     const interaction = await ai.interactions.create({
         model: "gemini-3.6-flash",
         input: [
-            { 
-                type: "text", text: "Caption this image." 
+            {
+                type: "text", text: "Caption this image."
             },
             {
                 type: "image",
                 data: base64ImageFile,
                 mime_type: "image/jpeg"
             }
-        ]
+        ],
+        system_instruction: `
+            You are an expert in generating caption for image.
+            You generate single caption for the image.
+            Your caption should be short and concise.
+            You use hashtags and emojis in the caption
+        `
     });
     console.log(interaction.output_text);
 
