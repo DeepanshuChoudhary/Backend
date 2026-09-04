36cd38d4b3b42a599f3884092875ec9abe8cec67
Deepanshu Choudhary <deepengineeryt@gmail.com>
2026-09-04 10:16:04 +0530

    Socket.io code

--- Socket.IO from YT/server/server.js
+++ Socket.IO from YT/server/server.js
@@ -1,5 +1,33 @@
 const app = require('./src/app')
+const { createServer } = require('http')
+const { Server } = require('socket.io')
 
-app.listen(3000, () => {
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ['GET', 'POST'],
+        credentials: true
+    }
+})
+
+io.on("connection", (socket) => {
+
+    console.log("User connected, ID : ", socket.id)
+
+    // socket.emit("welcome", `Welcome to the server`)
+    // socket.broadcast.emit("welcome", `Socket Id : ${socket.id} join this server`)
+
+    socket.on("message", (data) => {
+        console.log(data);
+    })
+
+    socket.on("disconnect", () => {
+        console.log("User Disconnected : ", socket.id)
+    })
+
+})
+
+httpServer.listen(3000, () => {
     console.log("Server running on port 3000")
 })
